da2223cc24db0083c035cb7183fc5c0b7241862c
Timothy <timothy@stagepage.ca>
2025-01-15 11:57:45 -0500

    added title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Board from './components/Board';
+import Title from './components/Title';
 
 const App = () => {
   return (
+    <div>
+      <Title />
       <Board />
+    </div>
   );
 };
 
---
+++ src/components/Title.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Title.css';
+
+const Title = () => {
+  return (
+    <div className="title-container">
+      <div className="title-box"> 
+        <h1 className="main-title">Chinese Checkers</h1>
+        <p className="subtitle">Long Jump Variation</p>
+        <p className="text">By Tim Cao</p>
+      </div>
+    </div>
+  );
+};
+
+export default Title;
